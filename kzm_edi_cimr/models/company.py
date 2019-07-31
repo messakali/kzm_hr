2c48d89a1f312e94cd36d5e6c89d999547d5798f
red <r.rouichi@karizma.ma>
2019-07-31 14:46:34 +0100

    [IMPL] T1237 - Rework 25/07

---
+++ kzm_edi_cimr/models/company.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class Company(models.Model):
+    _inherit = 'res.company'
+
+    # cimr = fields.Char(string=u'N° CIMR')
+    cimr_category = fields.Char(string=u'N° Catégorie CIMR')
