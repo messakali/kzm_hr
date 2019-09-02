0bd703d86cb979a997b902245c518fc7a25b3062
RED <r.rouichi@karizma.ma>
2019-09-02 16:37:19 +0100

    [IMPL] T1328 - Amelioration du module kzm_payroll_ma_reports

--- kzm_payroll_ma_reports/models/custom_company.py
+++ kzm_payroll_ma_reports/models/custom_company.py
@@ -3,10 +3,19 @@ from odoo import fields, models,api, osv
 from . import convertion
 
 
-class payroll_parametres(models.Model):
-    _inherit = 'hr.payroll_ma.parametres'
+class ResCompany(models.Model):
+    _inherit = 'res.company'
 
     banque_virement_paie = fields.Char(string='Banque')
     agence_virement_paie = fields.Char(string='Agence')
     ville_agence_virement_paie = fields.Char(string='Ville agence')
     rib_virement_paie = fields.Char(string=u'RIB')
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    banque_virement_paie = fields.Char(string='Banque',related="company_id.banque_virement_paie",  readonly=False)
+    agence_virement_paie = fields.Char(string='Agence',related="company_id.agence_virement_paie",  readonly=False)
+    ville_agence_virement_paie = fields.Char(string='Ville agence',related="company_id.ville_agence_virement_paie",  readonly=False)
+    rib_virement_paie = fields.Char(string='RIB',related="company_id.rib_virement_paie",  readonly=False)
